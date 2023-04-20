bbeb0e4d6c6d7563d7473731393065f78f5c2eed
Jaime A <jaimeangel1097@gmail.com>
2023-04-20 17:12:49 -0500

    creando el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,11 @@
-import { MARCAS } from "../constanst"
+import { MARCAS,AÑOS_SEGURO,PLANES} from "../constanst"
 
 function Formulario() {
   return (
     <>
         <form>
-            <div className="my-5">
+
+            <div className="my-4">
                 <label className="block mb-3 font-bold uppercase text-gray-400">Marca</label>
                 <select 
                     name="marca"
@@ -16,12 +17,56 @@ function Formulario() {
                             <option 
                                 key={marca.id}
                                 value={marca.id}
-                            >{marca.marca}</option>
+                            >
+                                {marca.marca}
+                            </option>
                         ))
                     }
                 </select>
 
             </div>
+
+            <div className="my-4">
+                <label className="block mb-3 font-bold uppercase text-gray-400">Año</label>
+                <select 
+                    name="marca"
+                    className="w-full p-3 bg-white border border-gray-200"
+                >
+                    <option value="">Seleccione un año</option>
+                    {
+                        AÑOS_SEGURO.map(año=>(
+                            <option 
+                                key={año}
+                                value={año}
+                            >
+                                {año}
+                            </option>
+                        ))
+                    }
+                </select>
+
+            </div>
+
+            <div className="my-4">
+                <label className="block mb-3 font-bold uppercase text-gray-400">Elige un plan</label>
+                <div className="flex gap-4">
+                    {
+                        PLANES.map(plan=>(
+                            <div key={plan.id} className="flex gap-2 items-center">
+                                <label>{plan.plan}</label>
+                                <input type="radio" name="plan" id={plan.id}/>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <input 
+                type="submit" 
+                value="Cotizar seguro"
+                className="w-full bg-gradient-to-r from-green-500 to-green-600 
+                        text-white py-2 rounded-lg cursor-pointer font-bold uppercase shadow-lg"
+            />
         </form>
       
     </>
--- src/constanst/index.js
+++ src/constanst/index.js
@@ -3,3 +3,11 @@ export const MARCAS=[
     {id:2,marca:'Americano'},
     {id:3,marca:'Asiatico'}
 ]
+
+const actual_year= new Date().getFullYear()
+export const AÑOS_SEGURO=Array.from(new Array(30),(_,index)=>actual_year-index)
+
+export const PLANES=[
+    {id:1,plan:'Básico'},
+    {id:2,plan:'Americano'}
+]
