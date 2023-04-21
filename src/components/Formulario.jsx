b46ef10e91eafca4c7a25f3ed995fa6f26a5eee7
Jaime A <jaimeangel1097@gmail.com>
2023-04-21 11:49:58 -0500

    agregando hook para eficiencia en el uso de context

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,78 +1,78 @@
-import { MARCAS,AÑOS_SEGURO,PLANES} from "../constanst"
-import CotizadorContext from "../context/CotizadorProvider"
-import { useContext } from "react"
+import {MARCAS,AÑOS_SEGURO,PLANES} from "../constanst"
+import useCotizador from "../hooks/useCotizador"
 
 function Formulario() {
-    const {hola}=useContext(CotizadorContext)
+
+    const {hola}=useCotizador()
     console.log(hola)
-  return (
-    <>
-        <form>
-            <div className="my-4">
-                <label className="block mb-3 font-bold uppercase text-gray-400">Marca</label>
-                <select 
-                    name="marca"
-                    className="w-full p-3 bg-white border border-gray-200"
-                >
-                    <option value="">Seleccione una marca</option>
-                    {
-                        MARCAS.map(marca=>(
-                            <option 
-                                key={marca.id}
-                                value={marca.id}
-                            >
-                                {marca.marca}
-                            </option>
-                        ))
-                    }
-                </select>
+    return(
+        <>
+            <form>
+                <div className="my-4">
+                    <label className="block mb-3 font-bold uppercase text-gray-400">Marca</label>
+                    <select 
+                        name="marca"
+                        className="w-full p-3 bg-white border border-gray-200"
+                    >
+                        <option value="">Seleccione una marca</option>
+                        {
+                            MARCAS.map(marca=>(
+                                <option 
+                                    key={marca.id}
+                                    value={marca.id}
+                                >
+                                    {marca.marca}
+                                </option>
+                            ))
+                        }
+                    </select>
 
-            </div>
-
-            <div className="my-4">
-                <label className="block mb-3 font-bold uppercase text-gray-400">Año</label>
-                <select 
-                    name="marca"
-                    className="w-full p-3 bg-white border border-gray-200"
-                >
-                    <option value="">Seleccione un año</option>
-                    {
-                        AÑOS_SEGURO.map(año=>(
-                            <option 
-                                key={año}
-                                value={año}
-                            >
-                                {año}
-                            </option>
-                        ))
-                    }
-                </select>
-
-            </div>
-
-            <div className="my-4">
-                <label className="block mb-3 font-bold uppercase text-gray-400">Elige un plan</label>
-                <div className="flex gap-4">
-                    {
-                        PLANES.map(plan=>(
-                            <div key={plan.id} className="flex gap-2 items-center">
-                                <label>{plan.plan}</label>
-                                <input type="radio" name="plan" id={plan.id}/>
-                            </div>
-                        ))
-                    }
                 </div>
-            </div>
 
-            <input 
-                type="submit" 
-                value="Cotizar seguro"
-                className="w-full bg-gradient-to-r from-green-500 to-green-600 
-                        text-white py-2 rounded-lg cursor-pointer font-bold uppercase shadow-lg"
-            />
-        </form>
-    </>
-  )
+                <div className="my-4">
+                    <label className="block mb-3 font-bold uppercase text-gray-400">Año</label>
+                    <select 
+                        name="marca"
+                        className="w-full p-3 bg-white border border-gray-200"
+                    >
+                        <option value="">Seleccione un año</option>
+                        {
+                            AÑOS_SEGURO.map(año=>(
+                                <option 
+                                    key={año}
+                                    value={año}
+                                >
+                                    {año}
+                                </option>
+                            ))
+                        }
+                    </select>
+
+                </div>
+
+                <div className="my-4">
+                    <label className="block mb-3 font-bold uppercase text-gray-400">Elige un plan</label>
+                    <div className="flex gap-4">
+                        {
+                            PLANES.map(plan=>(
+                                <div key={plan.id} className="flex gap-2 items-center">
+                                    <label>{plan.plan}</label>
+                                    <input type="radio" name="plan" id={plan.id}/>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+
+                <input 
+                    type="submit" 
+                    value="Cotizar seguro"
+                    className="w-full bg-gradient-to-r from-green-500 to-green-600 
+                            text-white py-2 rounded-lg cursor-pointer font-bold uppercase shadow-lg"
+                />
+            </form>
+        </>
+    )
 }
 
 export default Formulario
